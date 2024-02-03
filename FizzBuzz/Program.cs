47424fb678727d9b2f2af0d131b56a549a48dcb8
Soapy <soapytarantula@gmail.com>
2024-02-03 15:38:40 -0600

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class FizzBuzzProgram
+{
+    static void Main()
+    {
+        int userNum;
+        Console.WriteLine("Enter the number you wish to count to.");
+        Console.WriteLine();
+        try
+        {
+            userNum = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid input, did you input a number?");
+            throw;
+        }
+        Console.WriteLine();
+        for (int i = 1; i <= userNum; i++)
+        {
+            Console.WriteLine($"{i} is {FizzbuzzChecker(i)}");
+        }
+
+        static string FizzbuzzChecker(int number)
+        {
+            string _result;
+            if (number % 3 == 0 && number % 5 == 0) { _result = "Fizzbuzz"; } // If number is divisible by both 3 and 5 we have fizzbuzz.
+            else if (number % 5 == 0) { _result = "Buzz"; }
+            else if (number % 3 == 0) { _result = "Fizz"; }
+            else _result = "neither";
+            return _result;
+        }
+        Console.WriteLine();
+        Console.WriteLine("Any key to close.");
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Fizzbuzz-CS
+ Did you know that 420 is Fizzbuzz? Hell yeah.
